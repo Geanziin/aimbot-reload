0dcde3753be48b6de9e367b1604c06dc8c7327a1
Geanziin <gean.alvesaraujo@gmail.com>
2025-10-20 06:08:06 +0100

    private

--- WindowsFormsApp1/Spotify.cs
+++ WindowsFormsApp1/Spotify.cs
@@ -516,7 +516,7 @@ public class Spotify : Form
     this.txt3.Font = new Font("Microsoft Sans Serif", 14.5f);
     this.txt3.ForeColor = Color.FromArgb(147, 51, 234); // Roxo moderno
     this.txt3.HorizontalTextAlignment = Txt.HorizontalAlignment.Center;
-    this.txt3.Location = new Point(145, 96 /*0x60*/);
+    this.txt3.Location = new Point(138, 96 /*0x60*/);
     this.txt3.Name = "txt3";
     this.txt3.Size = new Size(48 /*0x30*/, 28);
     this.txt3.TabIndex = 3;
