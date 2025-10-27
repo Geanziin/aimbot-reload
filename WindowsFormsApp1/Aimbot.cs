28afa2f1f8aafaf63807db7f4b607dc0f29c3cfb
Geanziin <gean.alvesaraujo@gmail.com>
2025-10-27 21:09:55 +0000

    tmnc

--- WindowsFormsApp1/Aimbot.cs
+++ WindowsFormsApp1/Aimbot.cs
@@ -867,7 +867,7 @@ public class Aimbot : UserControl
     this.customCheckboxAimFov.Font = new Font("Microsoft Sans Serif", 12f, FontStyle.Regular, GraphicsUnit.Point, (byte) 0);
     this.customCheckboxAimFov.LabelColor = Color.White;
     this.customCheckboxAimFov.LabelFont = new Font("Microsoft Sans Serif", 11.8f);
-    this.customCheckboxAimFov.LabelSpacing = 83;
+    this.customCheckboxAimFov.LabelSpacing = 85;
     this.customCheckboxAimFov.LabelText = "Aim Fov - Risk";
     this.customCheckboxAimFov.Location = new Point(6, 117);
     this.customCheckboxAimFov.Name = "customCheckboxAimFov";
